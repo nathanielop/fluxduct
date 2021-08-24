22b5be18e5e152cc3f4f18d8572cdd76c05322cf
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-08-24 10:23:33 -0500

    Bug fix the bug fix

--- src/functions/evaluate-path.js
+++ src/functions/evaluate-path.js
@@ -3,12 +3,12 @@ export default (dictionary, path) => {
     throw new Error(`Expected array for path, received "${path}".`);
   }
   let resolvedValue = dictionary;
-  for (i in path) {
-    const newObj = resolvedValue[path[i]];
+  for (part of path) {
+    const newObj = resolvedValue[part];
     if (!newObj) {
-      throw new Error(`No value present in dictionary "${JSON.stringify(dictionary)}" for path "${JSON.stringify(path.slice(0, i))}".`);
+      throw new Error(`No value present in dictionary "${JSON.stringify(dictionary)}" for path "${JSON.stringify(path)}".`);
     }
-    resolvedValue = resolvedValue[path[i]];
+    resolvedValue = resolvedValue[part];
   }
   if (typeof resolvedValue !== 'number') {
     throw new Error(`Expected number value at path "${JSON.stringify(path.slice(0, i))}" in dictionary "${JSON.stringify(dictionary)}", received "${typeof resolvedValue}".`);
