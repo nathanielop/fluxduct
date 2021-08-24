efc80f3a21786ac831572022d958383068dfd928
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-08-24 11:03:17 -0500

    Fix test bug

--- src/functions/evaluate.test.js
+++ src/functions/evaluate.test.js
@@ -5,5 +5,5 @@ import evaluate from './evaluate.js';
 export default {
   'can evaluate': () => assert.equal(evaluate({ foo: 5, bar: { baz: 5 } }, { add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }] }), 10),
   'can evaluate nested operations': () => assert.equal(evaluate({ foo: 5, bar: { baz: 5 } }, { add: [{ path: ['foo'] }, { add: [{ path: ['bar', 'baz'] }, { path: ['foo'] }] }] }), 15),
-  'can evaluate nested conditions': () => assert.equal(evaluate({ foo: 5, bar: { baz: 5 }, test: 15, bool: false }, { add: [{ path: ['foo'] }, { if: [{ path: ['bool'] }, { path: ['bar', 'baz'] }, { path: 'test' }] }] }), 20),
+  'can evaluate nested conditions': () => assert.equal(evaluate({ foo: 5, bar: { baz: 5 }, test: 15, bool: false }, { add: [{ path: ['foo'] }, { if: [{ path: ['bool'] }, { path: ['bar', 'baz'] }, { path: ['test'] }] }] }), 20),
 }
