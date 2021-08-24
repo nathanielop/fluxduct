6476987b418ae221e9407ed4ea70f93b57e8e12c
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-08-24 10:42:55 -0500

    Update test

--- src/functions/parse.test.js
+++ src/functions/parse.test.js
@@ -4,5 +4,5 @@ import parse from './parse.js';
 
 export default {
   'can parse a single expression': () => assert.equal(parse({ foo: 5, bar: { baz: 5 } }, { add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }] }), 10),
-  'can parse multiple expressions':  () => assert.equal(parse({ foo: 5, bar: { baz: 5 }, bash: { git: { idno: 10 } } }, { add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }], multiply: [{ path: ['bash', 'git', 'idno'] }, { path: ['bar', 'baz'] }] }), [10, 50]),
+  'can parse multiple expressions':  () => assert.deepEqual(parse({ foo: 5, bar: { baz: 5 }, bash: { git: { idno: 10 } } }, { add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }], multiply: [{ path: ['bash', 'git', 'idno'] }, { path: ['bar', 'baz'] }] }), [10, 50]),
 }
