f164b7fbdbd512b67aad7ff88b61af74c4a83b86
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-08-24 10:29:47 -0500

    Update operator fn

--- src/constants/operator-functions.js
+++ src/constants/operator-functions.js
@@ -3,4 +3,9 @@ import subtract from '../functions/subtract.js';
 import multiply from '../functions/multiply.js';
 import divide from '../functions/divide.js';
 
-export default { add, subtract, multiply, divide }
+export default { 
+  add: (dictionary, args) => add(dictionary, args),
+  subtract: (dictionary, args) => subtract(dictionary, args),
+  multiply: (dictionary, args) => multiply(dictionary, args),
+  divide: (dictionary, args) => divide(dictionary, args),
+}
