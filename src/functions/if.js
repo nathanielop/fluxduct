cc13d4ffc5a42ef0efb34aa60a31347d12692430
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-08-24 10:49:51 -0500

    Dont overwrite predefined val

--- src/functions/if.js
+++ src/functions/if.js
@@ -1,7 +1,7 @@
 import evaluate from './evaluate.js';
 
 export default (dictionary, [a, ...args]) => {
-  const a = evaluate(dictionary, a, 'boolean');
+  a = evaluate(dictionary, a, 'boolean');
   const [b, c] = args.map(arg => evaluate(dictionary, arg));
   return a ? b : c;
 }
