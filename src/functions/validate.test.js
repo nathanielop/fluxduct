c40cc788cb52ce27a421595b7c6a6cf15b31ed28
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-10-12 13:06:58 -0500

    tweaks

--- src/functions/validate.test.js
+++ src/functions/validate.test.js
@@ -3,6 +3,6 @@ import { strict as assert } from 'assert';
 import validate from './validate.js';
 
 export default {
-  'can validate a single expression': () => assert.doesNotThrow(validate({ add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }] })),
-  'can validate nested expressions':  () => assert.doesNotThrow(validate({ add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }], multiply: [{ path: ['bash', 'git', 'idno'] }, { path: ['bar', 'baz'] }] })),
+  'can validate a single expression': () => assert.doesNotThrow(() => validate({ add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }] })),
+  'can validate nested expressions':  () => assert.doesNotThrow(() => validate({ add: [{ path: ['foo'] }, { path: ['bar', 'baz'] }], multiply: [{ path: ['bash', 'git', 'idno'] }, { path: ['bar', 'baz'] }] })),
 }
